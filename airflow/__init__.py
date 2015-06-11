c13ac0093a5bfea730b5984f26c8e896b40e77c0
Maxime Beauchemin <maximebeauchemin@gmail.com>
2015-06-10 23:09:22 -0700

    A simple plugin system for Airflow

--- airflow/__init__.py
+++ airflow/__init__.py
@@ -1,14 +1,16 @@
-__version__ = "1.0.1"
-
 """
 Authentication is implemented using flask_login and different environments can
 implement their own login mechanisms by providing an `airflow_login` module
 in their PYTHONPATH. airflow_login should be based off the
 `airflow.www.login`
 """
+__version__ = "1.0.1"
+
 import logging
 from airflow.configuration import conf
 from airflow.models import DAG
+from flask.ext.admin import BaseView
+
 
 from airflow import default_login as login
 if conf.getboolean('webserver', 'AUTHENTICATE'):
@@ -20,3 +22,13 @@ if conf.getboolean('webserver', 'AUTHENTICATE'):
             "authenticate is set to True in airflow.cfg, "
             "but airflow_login failed to import")
 
+
+class PluginView(BaseView):
+    pass
+
+from airflow import operators
+from airflow import hooks
+from airflow import executors
+
+operators.integrate_plugins()
+hooks.integrate_plugins()
